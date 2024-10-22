a1edda1f0facb3197a5987c0d93de6c13ff07435
karthik <karthik.nishanth06@gmail.com>
2024-10-22 14:28:17 +0100

    push

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,37 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10832930955"
+        />
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-10832930955');
+            `,
+          }}
+        />
+        <Script
+          defer
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+             n.queue=[];t=b.createElement(e);t.async=!0;
+             t.src=v;s=b.getElementsByTagName(e)[0];
+             s.parentNode.insertBefore(t,s)}(window, document,'script',
+                                             'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '362063942726861');
+            fbq('track', 'PageView');
+          `,
+          }}
+        />
+        <Script
           src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs"
           type="module"
         ></Script>
